d1f1d961387ea4dfa1cb492db5689b04203b9650
Charlie Clark <charlie.clark@clark-consulting.eu>
2012-07-21 15:02:50 +0200

    Apache licence removed. This is BSD. Extended docs_require

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python
 #
 # Copyright (C) 2012 Charlie Clark
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 """Python utility module Google Visualization Python API."""
 
@@ -32,7 +20,7 @@ if major == 2 and minor < 6:
 extra = dict(
     setup_requires=requires + ['setuptools'],
     tests_requires=requires + ['nose', 'coverage'],
-    doc_requires=requires + ['sphinx']
+    doc_requires=requires + ['sphinx', 'repoze.sphinx.autointerface']
     )
 
 setup(
@@ -41,7 +29,6 @@ setup(
     description="Python API for Google Visualization",
     long_description="""
     Date Table maps Python objects to the Google Visualization API
-
 """.strip(),
     author=__author__,
     license="BSD",
