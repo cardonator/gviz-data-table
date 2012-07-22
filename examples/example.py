fbfa6f1002f377bef428403c945a3c0261315593
Charlie Clark <charlie.clark@clark-consulting.eu>
2012-07-22 17:39:57 +0200

    Working demo of a bar chart.

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,58 @@
 import os
 import sqlite3
-from gviz_data_table import data_table
+from string import Template
 
-folder = os.path.split(__file__)[0]
-db = sqlite3.connect(os.path.join(folder, "sample.db"))
-c = db.cursor()
-c.execute("SELECT * FROM employees")
-cols = [col[0] for col in c.description]
+from gviz_data_table.data_table import Table, DataTableEncoder
+
+
+def data():
+    folder = os.path.split(__file__)[0]
+    db = sqlite3.connect(os.path.join(folder, "sample.db"))
+    c = db.cursor()
+    c.execute("SELECT name, salary FROM employees")
+    cols = [dict(id=col[0].capitalize(), type=col[1]) for col in c.description]
+    # sqlite3 unfortunately does not provide type information
+    cols[0]['type'] = unicode
+    cols[1]['type'] = float
+
+    t = Table(cols)
+    for r in c.fetchall():
+        t.append(r)
+
+    encoder = DataTableEncoder()
+    return encoder.encode(t)
+
+
+template = Template("""
+<html>
+
+<head>
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+</head>
+
+<body>
+<script>
+      google.load("visualization", "1", {packages:["corechart", "table"]});
+
+
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+
+      var data = new google.visualization.DataTable($data);
+      var chart = new google.visualization.BarChart(document.getElementById('chart'));
+      chart.draw(data);
+      }
+</script>
+<div id="chart"></div>
+
+</body>
+
+</html>
+""")
+
+def save():
+    with open("result.html", "wb") as f:
+        f.write(template.safe_substitute(data=data()))
+
+if __name__ == "__main__":
+    save()
