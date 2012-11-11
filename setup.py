c24be0c5feb7947971e88a8fed8006d8ca5a8af0
Charlie Clark <charlie.clark@clark-consulting.eu>
2012-11-11 15:04:57 +0100

    Updated configuration to be able to run tests.

--- setup.py
+++ setup.py
@@ -7,32 +7,45 @@
 __author__ = "Charlie Clark"
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 import sys
 
-requires = []
+requires = ['setuptools']
 major, minor = sys.version_info[:2]
 if major == 2 and minor < 6:
     requires.append('simplejson')
 
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 extra = dict(
-    testing=requires + ['nose', 'coverage'],
     docs=requires + ['sphinx', 'repoze.sphinx.autointerface']
     )
 
 setup(
-    name="gviz_data_table",
-    version="0.9.1",
-    description="Python API for Google Visualization",
-    long_description="""
+    name = "gviz_data_table",
+    version = "1.0.0",
+    description = "Python API for Google Visualization",
+    long_description = """
     Date Table maps Python objects to the Google Visualization API
 """.strip(),
-    author=__author__,
-    author_email='charlie.clark@clark-consulting.eu',
-    license="BSD",
-    url="https://bitbucket.org/charlie_x/gviz-data-table",
+    author = __author__,
+    author_email = 'charlie.clark@clark-consulting.eu',
+    license = "BSD",
+    url = "https://bitbucket.org/charlie_x/gviz-data-table",
     packages = find_packages(),
-    install_requires=requires,
-    tests_require=requires,
-    test_suite = 'gviz_data_table',
-    extras_require = extra
+    install_requires = requires,
+    tests_require = ['coverage', 'pytest', 'pytest-cov'],
+    test_suite ='gviz_data_table',
+    extras_require = extra,
+    cmdclass = {'test': PyTest},
 )
